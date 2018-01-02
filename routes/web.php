c482438d2e9609a4893008a7796cbe24e713bb29
adrianhp97 <13515091@std.stei.itb.ac.id>
2018-01-02 12:11:21 +0700

    change .htaccess

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+URL::forceSchema('https');
 /*
 |--------------------------------------------------------------------------
 | Client Pages
