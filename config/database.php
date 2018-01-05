4471945064784b2c4117d73b9395db0e5ecd46e6
adrianhp97 <13515091@std.stei.itb.ac.id>
2018-01-05 15:49:48 +0700

    postgres database

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,9 +58,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'shopee'),
+            'username' => env('DB_USERNAME', 'adrian'),
+            'password' => env('DB_PASSWORD', 'adrian'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
